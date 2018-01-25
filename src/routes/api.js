b9cd6f2fb24da0d7dbd901a25d23fcb5581beb63
Tabbish Noorani <tabishnoornai@gmail.com>
2018-01-26 01:26:26 +1000

    --

--- src/routes/api.js
+++ src/routes/api.js
@@ -22,15 +22,15 @@ Router.post('/imrego',(req, res)=>{
 
         new imrego({uid, title, catagory, description, imgURL})
         .save((err, rego)=>{
-            res.send({success: true, imNum: rego.imNum});
+            res.send({success: true, imrego: {...rego, _doc:{...rego._doc, uid:""}}});
         });
     } else res.send({success: false, msg: 'Invalid request body'})
 });
 
 Router.post('/fetch-item-lists',(req, res)=>{
     const {uid} = req.session.unsignedToken;
-// Note: send only activated onces.
-// Note: don't send the uid value.
+    // Note: send only activated onces.
+    // Note: don't send the uid value.
     imrego.find({uid: uid}).then((itemLists, err)=>{
         if (err) res.send({success: false, 
             msg:"Can't resolve the request. Please contact the webmasters."
@@ -38,5 +38,25 @@ Router.post('/fetch-item-lists',(req, res)=>{
         res.send({success: true, itemLists});
     })
 });
+
+Router.post('/delete-item-list', (req, res)=>{
+    const {uid} = req.session.unsignedToken;
+    
+    imrego.findById(req.body.id)
+    .then((data)=>{
+        if (data.uid==uid){
+            imrego.findByIdAndRemove(req.body.id)
+            .then((result)=>{
+                res.send({success: true, msg:"Deleted"})
+            })
+            .catch((err)=>{
+                res.send({success: false, msg:"Contact webmaster."})
+            })
+        } else {res.send({success: false, msg: "Not a valid request."})}
+    })
+    .catch((err)=>{
+        res.send({success: false, msg:"Contact webmaster."})
+    });
+})
 
 export default Router;
