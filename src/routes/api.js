1c8b65af8a0d3746a428d3d40189c93347fce19d
Tabbish Noorani <tabishnoornai@gmail.com>
2018-01-29 23:47:34 +1000

    image file upload done

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,21 +1,50 @@
 import express from 'express';
 import axios from 'axios';
+import multer from 'multer';
 import imrego from '../database/models/imrego';
 import config from '../config';
+import UUID from 'node-uuid';
 
 const Router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, './public/upload/')
+    },
+    filename: (req, file, cb)=>{
+        req.uuid = UUID.v4()
+        const FILE = (req.uuid + '-' + file.originalname);
+        cb(null, FILE);
+    }
+});
+
+const fileFilter =  (req, file, cb)=>{
+    if (file.mimetype==='image/jpeg' || file.mimetype==='image/png'){
+        cb(null, true)
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024*1024*5 // 5 megabytes,
+    },
+    fileFilter: fileFilter
+});
 
 Router.get('/', (req, res)=>{
     res.send({success: true, msg:`Welcome to server`});
 });
 
-Router.post('/imgupload', (req, res)=>{
-    //If it contains the file Uploads the file to cloudinary and return the url
-    //If no file then return default img URL from cloundinary 
-    res.send({success: true, url: 'https://res.cloudinary.com/oleaw/image/upload/v1516746659/DP_z4ail6.jpg'})
+Router.post('/imgupload',upload.single('itemImg') , (req, res)=>{
+    // res.send({success: true, url: 'https://res.cloudinary.com/oleaw/image/upload/v1516746659/DP_z4ail6.jpg'})   
+    res.send(req.file);
+
 });
 
-Router.post('/imrego',(req, res)=>{
+Router.post('/imrego', upload.single('itemImg'), (req, res)=>{
     if (req.body){
         const {uid} = req.session.unsignedToken;
         const {title, catagory, description, imgURL} = req.body;
